bd94fd26b8c3433acfd3d8fa9324024e485195ae
leaderfirestar <ewebb@factorearth.com>
2024-08-25 19:14:04 -0500

    Initial rich text renderer

---
+++ src/components/RichTextRenderer.tsx
@@ -0,0 +1,36 @@
+import { RichTextNode } from '@/lib/defintions';
+import React from 'react';
+
+interface RichTextRendererProps {
+	nodes: RichTextNode[];
+}
+
+function RichTextRenderer({ nodes }: RichTextRendererProps) {
+	function renderNode(node: RichTextNode, key: React.Key): React.ReactNode {
+		switch (node.type) {
+			case 'heading':
+				const HeadingTag = `h${node.level}` as keyof JSX.IntrinsicElements;
+				return <HeadingTag key={key}>{renderNodes(node.children)}</HeadingTag>;
+			case 'paragraph':
+				return <p key={key}>{renderNodes(node.children)}</p>;
+			case 'list':
+				const ListTag = node.format === 'ordered' ? 'ol' : 'ul';
+				return <ListTag key={key}>{renderNodes(node.children)}</ListTag>;
+			case 'list-item':
+				return <li key={key}>{renderNodes(node.children)}</li>;
+			case 'text':
+				return <span key={key}>{node.text}</span>;
+			default:
+				return null;
+		}
+	}
+
+	function renderNodes(nodes?: RichTextNode[]): React.ReactNode {
+		if (!nodes) return null;
+		return nodes.map((node, index) => renderNode(node, index));
+	}
+
+	return <>{renderNodes(nodes)}</>;
+}
+
+export default RichTextRenderer;
