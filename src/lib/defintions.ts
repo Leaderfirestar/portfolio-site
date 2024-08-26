92f7a1f38aac5fa2c4f19dc9ce2ba066883580f3
leaderfirestar <ewebb@factorearth.com>
2024-08-25 19:03:28 -0500

    Fixed definitions and added modules to get projects

---
+++ src/lib/defintions.ts
@@ -0,0 +1,80 @@
+interface Media {
+	id: number;
+	attributes: {
+		url: string;
+		formats?: {
+			thumbnail?: {
+				url: string;
+			};
+		};
+	};
+}
+
+export interface Project {
+	id: number;
+	title: string;
+	description: string;
+	slug: string;
+	image: {
+		data: Media | null;
+	};
+	gallery: {
+		data: Media[];
+	};
+	technologies: {
+		data: Technology[];
+	};
+	projectUrl: string;
+	sortIndex: number;
+}
+
+export interface Technology {
+	id: number;
+	name: string;
+	logo?: {
+		data: Media | null;
+	};
+}
+
+interface StrapiFindMeta {
+	page: number;
+	pageSize: number;
+	pageCount: number;
+	total: number;
+}
+
+interface StrapiFindErrorInterface {
+	status: number;
+	name: string;
+	message: string;
+	details: {};
+}
+
+interface StrapiFindError {
+	data: null;
+	error: StrapiFindErrorInterface;
+}
+
+interface StrapiFindList<T> {
+	data: T[];
+	meta: StrapiFindMeta;
+}
+
+export type StrapiFindResponse<T> = StrapiFindList<T> | StrapiFindError;
+
+interface StrapiFindThing<T> {
+	data: SingularThing<T>[];
+	meta: StrapiFindMeta;
+}
+
+interface StrapiFindThingError {
+	data: null;
+	error: StrapiFindErrorInterface;
+}
+
+export interface SingularThing<T> {
+	id: number;
+	attributes: T;
+}
+
+export type StrapiSingleThingResponse<T> = StrapiFindThing<T> | StrapiFindThingError;
---
+++ src/lib/projects.ts
@@ -0,0 +1,21 @@
+import { Project, SingularThing, StrapiFindResponse, StrapiSingleThingResponse } from "./defintions";
+
+export async function fetchProjects(): Promise<Project[]> {
+	const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/projects?populate=*`);
+	const json = await response.json() as StrapiFindResponse<Project>;
+	console.log('PROJECTS JSON', json);
+	if (json.data) return json.data;
+	console.error(json.error);
+	return [];
+}
+
+export async function fetchProject(id: string) { }
+
+export async function fetchProjectBySlug(slug: string): Promise<SingularThing<Project> | void> {
+	const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/projects?filters[slug][$eq]=${slug}&populate=*`);
+	const json = await response.json() as StrapiSingleThingResponse<Project>;
+	if (!json.data) {
+		console.error(json.error);
+	}
+	if (json.data && json.data.length > 0) return json.data[0];
+}
