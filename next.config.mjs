41af8a1d91ca9cd4a772c91be5b08b9dbc144120
leaderfirestar <ewebb@factorearth.com>
2024-08-25 19:03:00 -0500

    api endpoint env variable, allow images to be served from domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'http',
+				hostname: 'localhost',
+				port: '1337', // The port Strapi is running on
+				pathname: '/uploads/**', // Path pattern for Strapi images
+			}
+		]
+	}
+};
 
 export default nextConfig;
