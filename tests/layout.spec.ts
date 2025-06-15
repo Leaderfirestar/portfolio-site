99f8e6ec3d73cd8291e3f1484b88d9a8b44365e6
leaderfirestar <EricAWebb2000@yahoo.com>
2025-06-14 22:28:31 -0500

    Layout tests navbar/footer existence and verify navbar works on all pages

--- tests/layout.spec.ts
+++ tests/layout.spec.ts
@@ -3,37 +3,47 @@ import { loadEnvConfig } from "@next/env";
 
 loadEnvConfig(process.cwd());
 
-test("Navbar Works", async ({ page }) => {
-	await page.goto("/");
-	await page.getByRole("link", { name: "Home" }).click();
-	await expect(page).toHaveURL("/");
-	await page.getByRole("link", { name: "Projects" }).click();
-	await expect(page).toHaveURL("/projects");
-	await page.getByRole("link", { name: "Resume" }).click();
-	await expect(page).toHaveURL("/resume");
-});
+const pages = ["/", "/projects", "/resume"];
 
-test("Footer", async ({ page, request }) => {
-	await page.goto("/");
-
+test("Navbar and footer exist on all pages", async ({ page, request }) => {
 	// get personal info from the API
 	const response = await request.get(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/personal-info`);
 	expect(response.ok()).toBeTruthy();
 	const personalInfo = await response.json();
-
-	// Check for the mailto link in the footer
 	expect(personalInfo.data.email).toBeDefined();
-	await expect(page.getByRole("link", { name: personalInfo.data.email })).toHaveAttribute("href", `mailto:${personalInfo.data.email}`);
-
-	// Verify response returned first and last name
 	expect(personalInfo.data.firstName).toBeDefined();
 	expect(personalInfo.data.lastName).toBeDefined();
-
-	// Check for the GitHub link in the footer
 	expect(personalInfo.data.github).toBeDefined();
-	await expect(page.getByRole("link", { name: `${personalInfo.data.firstName} ${personalInfo.data.lastName}'s github` })).toHaveAttribute("href", personalInfo.data.github);
-
-	// Check for the LinkedIn link in the footer
 	expect(personalInfo.data.linkedin).toBeDefined();
-	await expect(page.getByRole("link", { name: `${personalInfo.data.firstName} ${personalInfo.data.lastName}'s linkedin` })).toHaveAttribute("href", personalInfo.data.linkedin);
+	for (const path of pages) {
+		await page.goto(path);
+
+		// Navbar
+		const nav = page.getByRole("navigation");
+		await expect(nav).toBeVisible();
+		await expect(nav.getByRole("link", { name: "Home" })).toBeVisible();
+		await expect(nav.getByRole("link", { name: "Projects" })).toBeVisible();
+		await expect(nav.getByRole("link", { name: "Resume" })).toBeVisible();
+
+		// Footer
+		const footer = page.getByRole("contentinfo");
+		await expect(footer).toBeVisible(); // ARIA role for footer
+		await expect(footer.getByRole("link", { name: personalInfo.data.email })).toHaveAttribute("href", `mailto:${personalInfo.data.email}`);
+		await expect(footer.getByRole("link", { name: `${personalInfo.data.firstName} ${personalInfo.data.lastName}'s github` })).toHaveAttribute("href", personalInfo.data.github);
+		await expect(footer.getByRole("link", { name: `${personalInfo.data.firstName} ${personalInfo.data.lastName}'s linkedin` })).toHaveAttribute("href", personalInfo.data.linkedin);
+	}
+});
+
+test("All nav links work on all pages", async ({ page }) => {
+	for (const from of pages) {
+		page.goto(from);
+		for (const to of pages) {
+			const name = to === "/" ? "Home" : to.slice(1).charAt(0).toUpperCase() + to.slice(2);
+			await page.getByRole("navigation").getByRole("link", { name }).click();
+			await expect(page).toHaveURL(to);
+			if (from !== page.url()) {
+				await page.goto(from);
+			}
+		}
+	}
 });
